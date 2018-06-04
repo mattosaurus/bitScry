d3b5eec544dd350c9e3428b5a9c87246b74eaf24
mattosaurus <mattosaurus@outlook.com>
2018-06-04 14:39:07 +0100

    Creation.

---
+++ bitScry/gulpfile.js
@@ -0,0 +1,75 @@
+﻿/// <binding BeforeBuild='clean, default' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rimraf = require("rimraf");
+var merge = require('merge-stream');
+var csso = require('gulp-csso');
+
+gulp.task("minify-js", function () {
+
+    var streams = [
+        gulp.src(["wwwroot/js/*.js"])
+            .pipe(uglify())
+            .pipe(concat("site.min.js"))
+            .pipe(gulp.dest("wwwroot/lib/site"))
+    ];
+
+    return merge(streams);
+});
+
+gulp.task("minify-css", function () {
+
+    return gulp.src('wwwroot/css/site.css')
+        .pipe(csso())
+        .pipe(concat("site.min.css"))
+        .pipe(gulp.dest('wwwroot/lib/site'))
+});
+
+gulp.task("minify", ['minify-js', 'minify-css']);
+
+// Dependency Dirs
+var deps = {
+    "bootstrap": {
+        "dist/**/*": ""
+    },
+    "jquery": {
+        "dist/*": ""
+    },
+    "popper.js": {
+        "dist/**/*": ""
+    }
+};
+
+gulp.task("clean-vendor", function (cb) {
+    return rimraf("wwwroot/vendor/", cb);
+});
+
+gulp.task("clean-site", function (cb) {
+    return rimraf("wwwroot/lib/site/", cb);
+});
+
+gulp.task("clean", ['clean-vendor', 'clean-site']);
+
+gulp.task("scripts", function () {
+
+    var streams = [];
+
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/vendor/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+
+});
+
+gulp.task("default", ['clean', 'minify', 'scripts']);
