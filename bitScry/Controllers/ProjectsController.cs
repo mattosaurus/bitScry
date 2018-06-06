02a158de006a2faf2f11c78d460ee5cb2799783a
mattosaurus <mattosaurus@outlook.com>
2018-06-06 11:45:08 +0100

    Added projects.

---
+++ bitScry/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bitScry.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
