f447eb6e2bf8319813d8bb88132061b732ff17ea
mattosaurus <mattosaurus@outlook.com>
2018-06-06 17:03:01 +0100

    Added weather DI.

--- bitScry/Controllers/Projects/WeatherController.cs
+++ bitScry/Controllers/Projects/WeatherController.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace bitScry.Controllers.Projects
 {
     [Route("Projects/[controller]")]
     public class WeatherController : Controller
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _config;
+
+        public WeatherController(IConfiguration config, IHttpContextAccessor httpContextAccessor)
+        {
+            _config = config;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
 
