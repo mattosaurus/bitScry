34506398924800914aba27648d057095de26d972
mattosaurus <mattosaurus@outlook.com>
2018-06-12 11:24:30 +0100

    Added NationalRail items.

---
+++ bitScry/AppCode/Projects/NationalRail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bitScry.AppCode.Projects
+{
+    public static class NationalRail
+    {
+    }
+}
