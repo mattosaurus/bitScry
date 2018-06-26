751d7e7a68c0363e737121e585423301b1b9d471
mattosaurus <mattosaurus@outlook.com>
2018-06-26 15:55:00 +0100

    Bing search controller added.

---
+++ bitScry/Controllers/Projects/BingSearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace bitScry.Controllers.Projects
+{
+    [Route("Projects/[controller]")]
+    public class BingSearchController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public BingSearchController(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
